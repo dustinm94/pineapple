0d52fee97bf6c39ae82bb1aa6b7f28f5e47c7d58
dustin <dmcallister9405@gmail.com>
2019-12-13 21:28:35 -0500

    returning session

--- payment_service/routes.py
+++ payment_service/routes.py
@@ -1,3 +1,4 @@
+from flask import request, json, jsonify
 from flask import current_app as app
 from flask_cors import cross_origin
 import stripe
@@ -9,16 +10,21 @@ stripe.api_key = 'sk_test_j1CvBmbOHBtYSwVpWG8ZnYc100ny492Yzc'
 @app.route('/api/pineapple/one_time_payment', methods=['POST'])
 @cross_origin()
 def one_time_payment():
-  session = stripe.checkout.Session.create(
-    payment_method_types=['card'],
-    line_items=[{
-      'name': 'T-shirt',
-      'description': 'Comfortable cotton t-shirt',
-      'images': ['https://example.com/t-shirt.png'],
-      'amount': 500,
-      'currency': 'usd',
-      'quantity': 1,
-    }],
-    success_url='https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-    cancel_url='https://example.com/cancel',
-  )
+    amount = request.json['amount']
+    name = request.json['name']
+
+    session = stripe.checkout.Session.create(
+      payment_method_types=['card'],
+      line_items=[{
+        'name': name,
+        'description': 'investment fund',
+        'images': ['https://example.com/t-shirt.png'],
+        'amount': amount,
+        'currency': 'usd',
+        'quantity': 1,
+      }],
+      success_url='https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url='https://example.com/cancel',
+    )
+
+    return jsonify(session), 200
